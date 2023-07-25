3329c414ba7a44528504655eafe2ea8446f2ccc7
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-07-25 20:21:14 +0200

    done the board

--- app.js
+++ app.js
@@ -3,5 +3,31 @@ const playerDisplay = document.querySelector("#player");
 const infoDisplay = document.querySelector("info-display");
 
 const startPieces = [
-    
+    rook, knight, bishop,queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook
 ]
+function createBoard() {
+    startPieces.forEach((startPieces, i) => {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.innerHTML = startPieces
+        square.setAttribute('square-id', i)
+      //  square.classList.add('beige')
+      const row = Math.floor((63 - i) / 8) + 1
+      if (row % 2 === 0) {
+        square.classList.add(i % 2 === 0 ? "beige" : "brown")
+      } else { 
+        square.classList.add(i % 2 === 0 ? "brown" : "beige")
+
+
+      }
+      gameBoard.append(square);
+    });
+}
+createBoard()
