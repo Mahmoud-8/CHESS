5ca3d02d61653f9d9db24aa6fc48d05705d11210
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-07-26 11:22:07 +0200

    done the board

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const gameBoard = document.querySelector("#gameboard");
 const playerDisplay = document.querySelector("#player");
 const infoDisplay = document.querySelector("info-display");
+const width = 8;
+let playerGo = 'black'
+playerDisplay.textContent = 'black'
 
 const startPieces = [
     rook, knight, bishop,queen, king, bishop, knight, rook,
@@ -13,21 +16,60 @@ const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook
 ]
 function createBoard() {
-    startPieces.forEach((startPieces, i) => {
+    startPieces.forEach((startPiece, i) => {
         const square = document.createElement('div');
         square.classList.add('square');
-        square.innerHTML = startPieces
+        square.innerHTML = startPiece
+        square.setAttribute('draggable', true)
         square.setAttribute('square-id', i)
-      //  square.classList.add('beige')
       const row = Math.floor((63 - i) / 8) + 1
       if (row % 2 === 0) {
         square.classList.add(i % 2 === 0 ? "beige" : "brown")
       } else { 
         square.classList.add(i % 2 === 0 ? "brown" : "beige")
 
-
       }
+        if ( i <= 15) {
+          square.classList.add('black')
+        }
+      
+        if ( i >= 48) {
+          square.classList.add('white')
+        }
       gameBoard.append(square);
-    });
+    })
 }
 createBoard()
+
+
+const allSquares = document.querySelectorAll("#gameboard .square")
+
+allSquares.forEach(square => {
+  square.addEventListener('dragstart', dragStart)
+  square.addEventListener('dragover', dragOver)
+  square.addEventListener('drop', dragDrop)
+
+})
+let startPostitionId;
+let draggedElement;
+
+function dragStart (e) {
+  startPostitionId = e.target.parentNode.getAttribute('square-id')
+  draggedElement = e.target
+}
+
+function dragOver(e) {
+  e.preventDefault()
+}
+function dragDrop(e) { 
+  e.stopPropagation()
+  const taken = e.target.classList.contains('peice')
+
+
+
+  //e.target.parentNode.append(draggedElement)
+  //e.target.remove()
+  //e.target.append(draggedElement)
+
+
+}
