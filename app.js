5fbcce60bd78e9a4b86dae5fcde06d13ff976126
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-07-26 17:07:30 +0200

    done the board

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ function createBoard() {
 createBoard()
 
 
-const allSquares = document.querySelectorAll("#gameboard .square")
+const allSquares = document.querySelectorAll(".square")
 
 allSquares.forEach(square => {
   square.addEventListener('dragstart', dragStart)
@@ -63,13 +63,50 @@ function dragOver(e) {
 }
 function dragDrop(e) { 
   e.stopPropagation()
+  const correctGo = draggedElement.firstChild.classList.contains(playerGo)
   const taken = e.target.classList.contains('peice')
+  const opponentGo = playerGo === 'white' ? 'black' : 'white'
+  const takenByOpponent = e.target.firstChild?.classList.contains(opponentGo)
+  
 
-
-
+  if (correctGo) {
+    if (takenByOpponent && valid) {
+      e.target.parentNode.append(draggedElement)
+      e.target.remove()
+      changePlayer()
+      return 
+    } 
+    if(taken) {
+      infoDisplay.textContent = "you cannot go here!"
+      setTimeout(() =>  infoDisplay.textContent = "", 2000)
+     
+      return
+    }
+  }
   //e.target.parentNode.append(draggedElement)
   //e.target.remove()
   //e.target.append(draggedElement)
 
+}
 
+function changePlayer() {
+  if(playerGo === "black") {
+    reverseIds()
+    playerGo = "white"
+    playerDisplay.textContent = 'white'
+  } else {
+    
+    playerGo = "black"
+    playerDisplay.textContent = 'black'
+  }
+}
+
+function reverseIds() {
+ const allSquares = document.querySelectorAll(".square")
+ allSquares.forEach((square, i) => square.setAttribute('square-id', (width * width -1) - i))
+}
+
+function revertIds() {
+  const allSquares = document.querySelectorAll(".square")
+  allSquares.forEach((square, i) => square.setAttribute('square-id', i))
 }
